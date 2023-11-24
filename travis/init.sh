f3258e771e9576e6a2d833155ec7f4c88e265f06
FrancescoRuscelli <francescoruscelli24@gmail.com>
2023-11-24 14:10:23 +0100

    adding all libboost

--- travis/init.sh
+++ travis/init.sh
@@ -3,8 +3,10 @@ set -ev
 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt update && sudo apt install libgtest-dev libyaml-cpp-dev swig python3-pip python3-setuptools liburdfdom-dev cmake libeigen3-dev libboost-filesystem-dev libboost-serialization-dev libboost-chrono-dev libblas-dev liblapack-dev patchelf python3-venv python3-wheel python3-nose libassimp-dev
-
+sudo apt update && sudo apt install libgtest-dev libyaml-cpp-dev python3-pip python3-setuptools liburdfdom-dev cmake libeigen3-dev \
+    libboost-all-dev \
+    libblas-dev liblapack-dev patchelf swig \
+    python3-venv python3-wheel python3-nose libassimp-dev
 
 if [ "$DISTRIBUTION" = "python" ]; then
     # prepare environment for python install
