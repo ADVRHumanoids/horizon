4e03af066990ab1d8d9f7e7d92b6452ceb6eb323
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-03-06 11:38:28 +0100

    added quat to rot in utils

--- horizon/utils/utils.py
+++ horizon/utils/utils.py
@@ -161,6 +161,52 @@ def rotationMatrixToQuaterion(R):
     q[2] = 0.5 * (R[1, 0] - R[0, 1]) / (4. * q[3])
     return q
 
+def quat_to_rot(quat):
+    """
+    Covert a quaternion into a full three-dimensional rotation matrix
+    [method specific for casadi function]
+
+    Input
+    :param quat: A 4 element array representing the quaternion (im(quat), re(quat))
+
+    Output
+    :return: A Casadi 3x3 element matrix representing the full 3D rotation matrix.
+             This rotation matrix converts a point in the local reference
+             frame to a point in the global reference frame.
+    """
+    # Extract the values from Q
+    q1 = quat[0]
+    q2 = quat[1]
+    q3 = quat[2]
+    q0 = quat[3]
+
+    # First row of the rotation matrix
+    r00 = 2 * (q0 * q0 + q1 * q1) - 1
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+
+    r0 = cs.horzcat(r00, r01, r02)
+
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = 2 * (q0 * q0 + q2 * q2) - 1
+    r12 = 2 * (q2 * q3 - q0 * q1)
+
+    r1 = cs.horzcat(r10, r11, r12)
+
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = 2 * (q0 * q0 + q3 * q3) - 1
+
+    r2 = cs.horzcat(r20, r21, r22)
+
+    # 3x3 rotation matrix
+    rot_matrix = cs.vertcat(r0, r1, r2)
+
+    return rot_matrix
+
+
 def double_integrator_with_floating_base(q, qdot, qddot, base_velocity_reference_frame = cas_kin_dyn.CasadiKinDyn.LOCAL):
     """
     Construct the floating-base dynamic model:
