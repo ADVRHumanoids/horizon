13996d7f933b19cd70daee9ed0ee07867434876c
FrancescoRuscelli <francescoruscelli24@gmail.com>
2024-03-25 17:57:38 +0100

    added quaternion multiply method in utils

--- horizon/utils/utils.py
+++ horizon/utils/utils.py
@@ -337,3 +337,11 @@ def barrier(x):
 def barrier1(x):
     return cs.if_else(x < 0, 0, x)
 
+def quaternion_multiply(q1, q2):
+    x1, y1, z1, w1 = q1
+    x2, y2, z2, w2 = q2
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2
+    z = w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2
+    return np.array([x, y, z, w])
