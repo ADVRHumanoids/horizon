2d7cabf708308a85ff2e96d83ae87b32f7f2c4e4
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-05 12:01:32 +0200

    now always returning list, fixed handling of subtasks for contact task

--- horizon/rhc/tasks/contactTask.py
+++ horizon/rhc/tasks/contactTask.py
@@ -12,9 +12,9 @@ class ContactTask(Task):
         establish/break contact
         """
 
-        self.dynamics_task: InteractionTask = Task.subtask_by_class(subtask, InteractionTask)
+        self.dynamics_tasks: InteractionTask = Task.subtask_by_class(subtask, InteractionTask)
         # allowed tasks are cartesian and rolling
-        self.kinematics_task: CartesianTask = Task.subtask_by_class(subtask, (CartesianTask, RollingTask)) # CartesianTask RollingTask
+        self.kinematics_tasks: CartesianTask = Task.subtask_by_class(subtask, (CartesianTask, RollingTask)) # CartesianTask RollingTask
 
         # initialize data class
         super().__init__(*args, **kwargs)
@@ -26,6 +26,8 @@ class ContactTask(Task):
         self.setNodes(self.nodes)
 
     def setNodes(self, nodes, erasing=True):
-
-        self.dynamics_task.setContact(nodes, erasing=erasing)  # this is from taskInterface
-        self.kinematics_task.setNodes(nodes, erasing=erasing)  # state + starting from node 1  # this is from taskInterface
+        
+        for task in self.dynamics_tasks:
+            task.setContact(nodes, erasing=erasing)  # this is from taskInterface
+        for task in self.kinematics_tasks:
+            task.setNodes(nodes, erasing=erasing)  # state + starting from node 1  # this is from taskInterface
