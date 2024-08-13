769a8e6c4fd80372c884dbd38321af841aeacd36
Andrea Patrizi <andreapatrizi1b6e6@gmail.com>
2024-08-13 11:13:36 +0200

    added method to get derivative of polynomial traj

--- horizon/utils/trajectoryGenerator.py
+++ horizon/utils/trajectoryGenerator.py
@@ -68,16 +68,66 @@ class TrajectoryGenerator:
 
         return y_bpoly
 
+    def derivative_of_trajectory(self, nodes, p_start, p_goal, clearance, derivatives=None):
+        if derivatives is None:
+            derivatives = [None] * len(p_start)
+
+        cxi = [0, 0.5, 1]
+        cyi = [p_start, p_goal + clearance, p_goal]
+
+        xcurve = np.linspace(0, 1, nodes)
+
+        yder = []
+        for i, val in enumerate(derivatives):
+            yder.append([cyi[i], val] if val is not None else [cyi[i]])
+
+        bpoly = BPoly.from_derivatives(cxi, yder)
+        
+        # Compute the derivative of the BPoly object
+        bpoly_derivative = bpoly.derivative()
+
+        # Evaluate the derivative at the given points
+        y_bpoly_derivative = bpoly_derivative(xcurve)
+
+        return y_bpoly_derivative
+    
 if __name__ == '__main__':
 
     tg = TrajectoryGenerator()
-    # #
-    # #
+
     n_samples = 100
-    # z_trj = tg.compute_polynomial_trajectory(0, range(n_samples), n_samples - 1, [0, 0, 0], [0, 0, 0], 1, dim=2)
-    z_trj = tg.from_derivatives(n_samples, -1, -1, 1, derivatives=[None, 0, None])
+    derivatives=[None, 0, 0]
+    start_pos = -1
+    end_pos = -1
+    height = 1
+    # Original trajectory
+    z_trj = tg.from_derivatives(n_samples, start_pos, end_pos, height, derivatives=derivatives)
+
+    # Derivative of the trajectory
+    z_trj_derivative = tg.derivative_of_trajectory(n_samples, start_pos, end_pos, height, derivatives=derivatives)
+
     axis = np.linspace(0, 1, num=z_trj.shape[0])
-    print(z_trj)
-    plt.plot(axis, z_trj)
+
+    # Plotting the trajectory
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(axis, z_trj, label="Trajectory")
+    plt.title("Trajectory")
+    plt.xlabel("Time (normalized)")
+    plt.ylabel("Position")
     plt.grid()
+    plt.legend()
+
+    # Plotting the derivative of the trajectory
+    plt.subplot(1, 2, 2)
+    plt.plot(axis, z_trj_derivative, label="Trajectory Derivative", linestyle="--")
+    plt.title("Derivative of Trajectory")
+    plt.xlabel("Time (normalized)")
+    plt.ylabel("Velocity")
+    plt.grid()
+    plt.legend()
+
+    plt.tight_layout()
     plt.show()
+
