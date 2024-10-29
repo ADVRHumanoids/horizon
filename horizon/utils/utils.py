74aaa8e5051e568b6e95a7f41c54f361d2985275
EnricoMingo <enricomingo@gmail.com>
2024-10-29 10:37:04 +0100

    added computation of quaternion error

--- horizon/utils/utils.py
+++ horizon/utils/utils.py
@@ -73,6 +73,22 @@ def quaterion_product(q, p):
 
     return [q0*p[0:3] + p0*q[0:3] + cs.mtimes(cs.skew(q[0:3]), p[0:3]), q0*p0 - cs.mtimes(q[0:3].T, p[0:3])]
 
+def quaternion_error(qx, qy, qz, qw,
+                     qx_d, qy_d, qz_d, qw_d):
+    """
+    Compute quaternion error between two quaternions
+    Args:
+        qx, qy, qz, qw: quaternion
+        qx_d, qy_d, qz_d, qw_d: desired quaternion
+
+    Returns:
+        quaternion error
+    """
+    eps = [qx, qy, qz]
+    eps_d = [qx_d, qy_d, qz_d]
+
+    return qw * eps_d - qw_d * eps - cs.mtimes(cs.skew(eps_d), eps)
+
 def toRot2(qx, qy, qz, qw):
     return toRot([qx, qy, qz, qw])
 
