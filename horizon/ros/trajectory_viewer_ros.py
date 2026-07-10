f36b1adc15428d0976aa66e2f02f3e1d06519895
Luca Rossini <luca.rossini@iit.it>
2026-07-10 16:52:11 +0200

    porting branch `devel` to ROS2

--- horizon/ros/trajectory_viewer_ros.py
+++ horizon/ros/trajectory_viewer_ros.py
@@ -1,15 +1,20 @@
 import argparse
-import rospy
 from visualization_msgs.msg import Marker
+
+from horizon.ros import ros2
+from horizon.ros.trajectory_viewer import TrajectoryViewer
 
 def main(args):
 
+    ros2.init_node("trajectory_viewer")
     frame = args.frame
-    rate = args.rate
+    rate = float(args.rate)
+    markers_max = int(args.markers_max)
+    pub = TrajectoryViewer(frame)
     k = 0
 
-    rospy_rate = rospy.Rate(rate)
-    while True:
+    rospy_rate = ros2.Rate(rate)
+    while not ros2.is_shutdown():
         if k == markers_max:
             action = Marker.DELETEALL
             k = 0
@@ -18,7 +23,7 @@ def main(args):
             action = Marker.ADD
 
         k += 1
-        pub.publish_once(action=action, markers_max=markers_max)
+        pub.publish_sphere(action=action, markers_max=markers_max)
         rospy_rate.sleep()
 
 if __name__ == '__main__':
@@ -26,8 +31,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
         description='')
-    parser.add_argument('--frame', '-f', help='frame')
-    parser.add_argument('--rate', '-r', help='rate')
+    parser.add_argument('--frame', '-f', default='world', help='frame')
+    parser.add_argument('--rate', '-r', default=10.0, help='rate')
+    parser.add_argument('--markers-max', '-m', default=1000, help='maximum markers before reset')
 
     args = parser.parse_args()
     main(args)
